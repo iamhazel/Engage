31b2f825c3b08daf64e666a48a35c81fe07e9bba
iamhazel <heyzel.james@gmail.com>
2023-04-01 16:03:02 -0400

    Replaced custom alerts with WinUI InfoBars (not rendering yet). Added Layouts page (still crashing on nav).

--- Engage/Helpers/DependencyInjection.cs
+++ Engage/Helpers/DependencyInjection.cs
@@ -9,25 +9,24 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
 
 public static class DependencyInjection
 {
     // Extension method for adding application services to the service collection
     public static void AddApplicationServices(this ServiceCollection services)
     {
-        // Add the ApiClient and Service objects to the service collection as singletons
-        services.AddSingleton<IApiClient, ApiClient>();
-        services.AddSingleton<IChatService, ChatService>();
+        services.AddSingleton<MainWindow>();
+        services.AddSingleton<Func<Frame>>(sp => () => ((MainWindow)sp.GetService<MainWindow>()).PublicSignalContainer);
 
-        // Add the ChatViewModel and SettingsViewModel objects to the service collection as transient
+        services.AddTransient<SettingsViewModel>();
         services.AddTransient<ChatViewModel>();
         services.AddTransient<ChatTabViewModel>();
-        services.AddTransient<SettingsViewModel>();
+        services.AddSingleton<SignalManager>();
 
-        // Add the MainWindow object to the service collection as a singleton
-        services.AddSingleton<MainWindow>();
-
-        // Add the HttpClient and logging services to the service collection
+        services.AddSingleton<IChatService, ChatService>();
+        services.AddSingleton<IApiClient, ApiClient>();
         services.AddHttpClient<IApiClient, ApiClient>(client =>
         {
             client.BaseAddress = new Uri("https://api.openai.com/v1/");
@@ -35,10 +34,8 @@ public static class DependencyInjection
         services.AddLogging(builder =>
         {
             builder.AddConsole();
-            builder.SetMinimumLevel(LogLevel.Information); // Make sure this is LogLevel.Information or lower
+            builder.SetMinimumLevel(LogLevel.Information);
         });
-
-        // Add helper services
         services.AddSingleton<ILocalSettingsService, LocalSettingsService>();
     }
 }
---
+++ Engage/Views/Layouts/MainWindowLayout.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Engage.Views.Layouts
+{
+    public sealed partial class MainWindowLayout : UserControl
+    {
+        public MainWindowLayout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
