a211d5d893825d1e894256cf458c0391d47b1a69
iamhazel <heyzel.james@gmail.com>
2023-04-05 23:28:19 -0400

    Save point! Settings cards are working.

--- Engage/Views/Controls/SettingsCardControl.xaml.cs
+++ Engage/Views/Controls/SettingsCardControl.xaml.cs
@@ -1,24 +1,15 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using Engage.Views.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 
+// Engage.Views.Controls.SettingsCardControl.xaml.cs
 namespace Engage.Views.Controls
 {
     public partial class SettingsCardControl : UserControl, INotifyPropertyChanged
     {
-        public static readonly DependencyProperty CardWidthProperty =
-            DependencyProperty.Register("CardWidth", typeof(double), typeof(SettingsCardControl), new PropertyMetadata(800.0));
-
-        public double CardWidth
-        {
-            get { return (double)GetValue(CardWidthProperty); }
-            set { SetValue(CardWidthProperty, value); }
-        }
-
         public static readonly DependencyProperty CardBackgroundProperty =
             DependencyProperty.Register("CardBackground", typeof(Brush), typeof(SettingsCardControl), new PropertyMetadata(null));
 
@@ -27,6 +18,7 @@ namespace Engage.Views.Controls
             get { return (Brush)GetValue(CardBackgroundProperty); }
             set { SetValue(CardBackgroundProperty, value); }
         }
+
 
         public static readonly DependencyProperty CardBorderBrushProperty =
             DependencyProperty.Register("CardBorderBrush", typeof(Brush), typeof(SettingsCardControl), new PropertyMetadata(null));
@@ -37,23 +29,6 @@ namespace Engage.Views.Controls
             set { SetValue(CardBorderBrushProperty, value); }
         }
 
-        public static readonly DependencyProperty CardBorderThicknessProperty =
-            DependencyProperty.Register("CardBorderThickness", typeof(Thickness), typeof(SettingsCardControl), new PropertyMetadata(new Thickness(1)));
-
-        public Thickness CardBorderThickness
-        {
-            get { return (Thickness)GetValue(CardBorderThicknessProperty); }
-            set { SetValue(CardBorderThicknessProperty, value); }
-        }
-
-        public static readonly DependencyProperty CardCornerRadiusProperty =
-            DependencyProperty.Register("CardCornerRadius", typeof(CornerRadius), typeof(SettingsCardControl), new PropertyMetadata(new CornerRadius(4)));
-
-        public CornerRadius CardCornerRadius
-        {
-            get { return (CornerRadius)GetValue(CardCornerRadiusProperty); }
-            set { SetValue(CardCornerRadiusProperty, value); }
-        }
 
         public static readonly DependencyProperty IconGlyphProperty =
             DependencyProperty.Register("IconGlyph", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
@@ -64,14 +39,16 @@ namespace Engage.Views.Controls
             set { SetValue(IconGlyphProperty, value); }
         }
 
+
         public static readonly DependencyProperty IconFontSizeProperty =
-            DependencyProperty.Register("IconFontSize", typeof(double), typeof(SettingsCardControl), new PropertyMetadata(24.0));
+            DependencyProperty.Register("IconFontSize", typeof(double), typeof(SettingsCardControl), new PropertyMetadata(null));
 
         public double IconFontSize
         {
             get { return (double)GetValue(IconFontSizeProperty); }
             set { SetValue(IconFontSizeProperty, value); }
         }
+
 
         public static readonly DependencyProperty CardTitleProperty =
             DependencyProperty.Register("CardTitle", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
@@ -82,67 +59,16 @@ namespace Engage.Views.Controls
             set { SetValue(CardTitleProperty, value); }
         }
 
-        public static readonly DependencyProperty CardSubtitleProperty =
-            DependencyProperty.Register("CardSubtitle", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null, OnCardSubtitlePropertyChanged));
 
-        private static void OnCardSubtitlePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var control = d as SettingsCardControl;
-            if (control != null)
-            {
-                control.OnCardSubtitleChanged(e.NewValue as string);
-            }
-        }
+        public static readonly DependencyProperty CardSubtitleProperty =
+            DependencyProperty.Register("CardSubtitle", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
+
         public string CardSubtitle
         {
             get { return (string)GetValue(CardSubtitleProperty); }
-            set { SetValue(CardSubtitleProperty, value); OnPropertyChanged(); }
+            set { SetValue(CardSubtitleProperty, value); }
         }
 
-        public static readonly DependencyProperty ToggleSwitchWidthProperty =
-            DependencyProperty.Register("ToggleSwitchWidth", typeof(double), typeof(SettingsCardControl), new PropertyMetadata(110.0));
-
-        public double ToggleSwitchWidth
-        {
-            get { return (double)GetValue(ToggleSwitchWidthProperty); }
-            set { SetValue(ToggleSwitchWidthProperty, value); }
-        }
-
-        public static readonly DependencyProperty ToggleSwitchMarginProperty =
-            DependencyProperty.Register("ToggleSwitchMargin", typeof(Thickness), typeof(SettingsCardControl), new PropertyMetadata(new Thickness(6, 0, 0, 0)));
-
-        public Thickness ToggleSwitchMargin
-        {
-            get { return (Thickness)GetValue(ToggleSwitchMarginProperty); }
-            set { SetValue(ToggleSwitchMarginProperty, value); }
-        }
-
-        public static readonly DependencyProperty ToggleSwitchOnContentProperty =
-            DependencyProperty.Register("ToggleSwitchOnContent", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
-
-        public string ToggleSwitchOnContent
-        {
-            get { return (string)GetValue(ToggleSwitchOnContentProperty); }
-            set { SetValue(ToggleSwitchOnContentProperty, value); }
-        }
-
-        public static readonly DependencyProperty ToggleSwitchOffContentProperty =
-            DependencyProperty.Register("ToggleSwitchOffContent", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
-
-        public string ToggleSwitchOffContent
-        {
-            get { return (string)GetValue(ToggleSwitchOffContentProperty); }
-            set { SetValue(ToggleSwitchOffContentProperty, value); }
-        }
-
-        public static readonly DependencyProperty ToggleSwitchIsEnabledProperty =
-            DependencyProperty.Register("ToggleSwitchIsEnabled", typeof(bool), typeof(SettingsCardControl), new PropertyMetadata(true));
-
-        public bool ToggleSwitchIsEnabled
-        {
-            get { return (bool)GetValue(ToggleSwitchIsEnabledProperty); }
-            set { SetValue(ToggleSwitchIsEnabledProperty, value); }
-        }
 
         public static readonly DependencyProperty ClickGlyphProperty =
             DependencyProperty.Register("ClickGlyph", typeof(string), typeof(SettingsCardControl), new PropertyMetadata(null));
@@ -153,16 +79,19 @@ namespace Engage.Views.Controls
             set { SetValue(ClickGlyphProperty, value); }
         }
 
+
         public SettingsCardControl()
         {
             InitializeComponent();
         }
+
 
         protected override void OnPointerEntered(PointerRoutedEventArgs e)
         {
             base.OnPointerEntered(e);
             VisualStateManager.GoToState(this, "PointerOver", true);
         }
+
 
         protected override void OnPointerExited(PointerRoutedEventArgs e)
         {
@@ -176,21 +105,5 @@ namespace Engage.Views.Controls
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        private void OnCardSubtitleChanged(string newValue)
-        {
-            if (string.IsNullOrEmpty(newValue))
-            {
-                // hide the CardSubtitle TextBlock if the value is null or empty
-                CardSubtitleTextBlock.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                // show the CardSubtitle TextBlock and update its text
-                CardSubtitleTextBlock.Visibility = Visibility.Visible;
-                CardSubtitleTextBlock.Text = newValue;
-            }
-        }
-
     }
 }
--- Engage/Views/SettingsPage.xaml.cs
+++ Engage/Views/SettingsPage.xaml.cs
@@ -1,11 +1,23 @@
+using System.Diagnostics;
+using Engage.Views.Models;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
+// Engage.Views.SettingsPage.xaml.cs
 namespace Engage.Views;
 
 public sealed partial class SettingsPage : Page
 {
+    public SettingsViewModel ViewModel { get; } = new SettingsViewModel();
+
     public SettingsPage()
     {
         InitializeComponent();
+        DataContext = ViewModel;
+    }
+
+    private void DebugButton_Click(object sender, RoutedEventArgs e)
+    {
+        Debug.WriteLine($"SettingCards count: {ViewModel.SettingCards.Count}");
     }
 }
